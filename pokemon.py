bdcd828c63b59ef61a8debfc33e81a16f7571132
Tej Allada <tejallada@gmail.com>
2025-05-09 23:42:28 -0400

    added comments for design of the attack method

--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,17 @@
+#This file contains the Pokemon class which is a class that gives the attributes of a pokemon
+
 class Pokemon:
+
+    #initializes the pokemon
     def __init__(self, name, type):
         self.name = name
         self.type = type
         self.level = 1
         fight_options = [attack, dodge, heal]
 
-    def attack(self, move):
-        if move = attack
+    #if the pokemon attacks
+    def attack(self, other):
+
+        #the other pokemon should have a random
+        #chance that it gets hit here based on the level
+#       of your pokemon and the level of that pokemon
